1d0c7053dcec0d2247b34d90fc78f5ac29d9e913
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-06 19:20:19 +0300

    I added new Java class SearchResultsPage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,9 +1,21 @@
 package pages;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
     @FindBy(css = "[name='q']")
     private WebElement searchInput;
+
+    public HomePage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    public void performSearch(String searchPhrase){
+        searchInput.sendKeys(searchPhrase);
+        searchInput.sendKeys(Keys.ENTER);
+    }
 }
