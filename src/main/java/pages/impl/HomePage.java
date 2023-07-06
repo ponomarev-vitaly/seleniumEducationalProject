d96c4bf6a43d1f09c3ebb9a5f76e4dd6b67a6a37
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-07 00:15:44 +0300

    I checked the code and run the test successfully.

--- src/main/java/pages/impl/HomePage.java
+++ src/main/java/pages/impl/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import pages.WebPage;
 
 public class HomePage extends WebPage {
     @FindBy(css = "[name='q']")
