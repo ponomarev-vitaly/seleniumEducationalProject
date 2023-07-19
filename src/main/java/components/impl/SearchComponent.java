eb2b6b0f1fed15fc0f488954bfade0522706d626
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-19 18:24:10 +0300

    I added constructor in the WebComponent java class.

--- src/main/java/components/impl/SearchComponent.java
+++ src/main/java/components/impl/SearchComponent.java
@@ -1,7 +1,11 @@
 package components.impl;
 
 import components.WebComponent;
+import org.openqa.selenium.WebElement;
 
 public class SearchComponent extends WebComponent {
 
+    protected SearchComponent(WebElement rootElement) {
+        super(rootElement);
+    }
 }
