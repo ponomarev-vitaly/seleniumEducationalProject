5aedbc5b422df6c5d9438deb3b112c68d0a3c58e
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-19 18:34:12 +0300

    I added SearchResultItemComponent java class and added constructor in it.

---
+++ src/main/java/components/impl/SearchResultItemComponent.java
@@ -0,0 +1,10 @@
+package components.impl;
+
+import components.WebComponent;
+import org.openqa.selenium.WebElement;
+
+public class SearchResultItemComponent extends WebComponent {
+    public SearchResultItemComponent(WebElement rootElement){
+        super(rootElement);
+    }
+}
