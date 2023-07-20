b5bd0ad10724d770c6d58f582603d6a8e114ed00
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-21 00:07:44 +0300

    I added 2 variables title and description, and created constructor as well.

--- src/main/java/entities/SearchResultItem.java
+++ src/main/java/entities/SearchResultItem.java
@@ -1,4 +1,14 @@
 package entities;
 
 public class SearchResultItem {
+    private final String title;
+    private final String description;
+
+
+    public SearchResultItem(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+
 }
